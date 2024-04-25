944bd04b4e9d96df9337f5ab44ff6487788b069d
maurice diong <mdiong00@gmail.com>
2024-04-24 20:25:28 -0400

    initial commit

---
+++ server/migrations/versions/56968e90b718_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 56968e90b718
+Revises: 
+Create Date: 2024-04-24 20:25:11.584299
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56968e90b718'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
