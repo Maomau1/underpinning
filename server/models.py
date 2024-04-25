7a541524fbbef278436c6b3631ce8b7cb6f42e72
maurice diong <mdiong00@gmail.com>
2024-04-24 20:48:19 -0400

    generate project

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+
+from config import db, bcrypt
+
+class Project(db.Model, SerializerMixin):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    
